b31359d4b8e4a452f7bfe2f4fff2c4448bd7fab5
yusragcelikel <yusragcelikel@gmail.com>
2024-09-09 11:31:11 +0300

    Revert "some update"
    This reverts commit 3cb9056302d028604a1cf6f7bc6f3b619c4348c9.

--- apps/home/models.py
+++ apps/home/models.py
@@ -11,11 +11,6 @@ class Customer(db.Model):
     kkb = db.Column(db.Integer, nullable=False)
     m1 = db.Column(db.Integer, nullable=False)
     m2 = db.Column(db.Integer, nullable=False)
-    country_of_residence = db.Column(db.String(64), nullable=False)
-    citizenship = db.Column(db.String(64), nullable=False)
-    is_politician = db.Column(db.Boolean, nullable=False)
-    has_political_affiliation = db.Column(db.Boolean, nullable=False)
-    # Diğer alanlar burada tanımlanacak
 
     def __repr__(self):
         return f'<Customer {self.name} {self.surname}>'
@@ -31,11 +26,6 @@ class FinancialInformation(db.Model):
     loans = db.Column(db.Integer, nullable=False)
     savings = db.Column(db.Float, nullable=False)
     investments = db.Column(db.Float, nullable=False)
-    loan_purpose = db.Column(db.String(64), nullable=False)
-    loan_branch = db.Column(db.String(64), nullable=False)
-    number_of_loans = db.Column(db.Integer, nullable=False)
-    loan_month = db.Column(db.String(20), nullable=False)
-    # Diğer alanlar burada tanımlanacak
 
     customer = db.relationship('Customer', backref=db.backref('financial_info', lazy=True))
 
